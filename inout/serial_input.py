e6d57f87a210743e69a416c46f96db94a5a63140
a123490 <gjhgogo2008@gmail.com>
2025-11-15 16:11:35 +0900

    main 제외한 각 파일 모듈들 버그 수정 및 코드 완성

--- inout/serial_input.py
+++ inout/serial_input.py
@@ -4,17 +4,13 @@
 import serial
 import time
 
-#연결 포트 COM3, 1초동안 데이터 없으면 종료
-ser = serial.Serial('COM3', 9600, timeout = 1)
-
-#아두이노 리셋 기다리기
-time.sleep(2)
+import serial
+import time
 
 #데이터를 읽어 문자로 정리
-def get_temhumgas():
+def get_temhumgas(ser):
   if ser.in_waiting>0:
     line = ser.readline().decode('utf-8').strip()
-    print("받은 원본 데이터:", line)
     
     values = line.split(',')
     if len(values) == 3:
@@ -23,3 +19,17 @@ def get_temhumgas():
       gas = int(values[2])
       return temperature, humidity, gas
     return None, None, None
+
+if __name__ == "__main__" :
+  #연결 포트 COM3, 1초동안 데이터 없으면 종료
+  ser = serial.Serial('COM3', 9600, timeout = 1)
+
+  #아두이노 리셋 기다리기
+  time.sleep(2)
+
+  while True :
+    temhumgas = get_temhumgas(ser)
+    if temhumgas != None:
+      print(temhumgas)
+    else:
+      pass
--- inout/serial_output.py
+++ inout/serial_output.py
@@ -1,29 +1,47 @@
 '''출력값 보내는 코드
 정재균 담당'''
+
 import serial
 import time
 
-ser=serial.Serial('COM3',9600,timeout=1)
+def open_window(ser):
+    ser.write('WIN_ON\n'.encode('utf-8'))
 
-time.sleep(2)
+def close_window(ser):
+    ser.write('WIN_OFF\n'.encode('utf-8'))
 
-def open_window():
-    ser.write('WIN_ON\n')
+def sp_aircon(ser):
+    ser.write('FAN\n'.encode('utf-8'))
 
-def close_window():
-    ser.write('WIN_OFF\n')
+def js_aircon(ser):
+    ser.write('DRY\n'.encode('utf-8'))
 
-def sp_aircon():
-    ser.write('FAN\n')
+def hon_aircon(ser):
+    ser.write('HEAT\n'.encode('utf-8'))
 
-def js_aircon():
-    ser.write('DRY\n')
+def con_aircon(ser):
+    ser.write('COOL\n'.encode('utf-8'))
 
-def hon_aircon():
-    ser.write('HEAT\n')
+def off_aircon(ser):
+    ser.write('OFF\n'.encode('utf-8'))
 
-def con_aircon():
-    ser.write('COOL\n')
+if __name__ == "__main__" :
+  #연결 포트 COM3, 1초동안 데이터 없으면 종료
+  ser = serial.Serial('COM3', 9600, timeout = 1)
 
-def off_aircon():
-    ser.write('OFF\n')
+  #아두이노 리셋 기다리기
+  time.sleep(2)
+
+  open_window(ser)
+  time.sleep(1)
+  close_window(ser)
+  time.sleep(1)
+  sp_aircon(ser)
+  time.sleep(1)
+  js_aircon(ser)
+  time.sleep(1)
+  off_aircon(ser)
+  time.sleep(1)
+  con_aircon(ser)
+  time.sleep(1)
+  hon_aircon(ser)

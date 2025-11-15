86f0a765e912f906d9d072cb138f4e4a55a4d6ad
arim0124 <rimasung0124@gmail.com>
2025-11-15 13:33:42 +0900

    정재균 류혜윤 코드(수정X)

--- inout/serial_output.py
+++ inout/serial_output.py
@@ -1,2 +1,29 @@
 '''출력값 보내는 코드
 정재균 담당'''
+import serial
+import time
+
+ser=serial.Serial('COM3',9600,timeout=1)
+
+time.sleep(2)
+
+def open_window():
+    ser.write('WIN_ON\n')
+
+def close_window():
+    ser.write('WIN_OFF\n')
+
+def sp_aircon():
+    ser.write('FAN\n')
+
+def js_aircon():
+    ser.write('DRY\n')
+
+def hon_aircon():
+    ser.write('HEAT\n')
+
+def con_aircon():
+    ser.write('COOL\n')
+
+def off_aircon():
+    ser.write('OFF\n')
--- utils/weather.py
+++ utils/weather.py
@@ -1,2 +1,19 @@
 '''오늘 미세먼지 농도 불러오는 애
 류혜윤 담당 '''
+import requests as rq
+
+def get_dust() : 
+
+    MY_KEY = 'c65a493de678400bd8136f3cae8817528a1faecd0a4238f9f46a0ba85c1b1370'
+
+    url = f'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey={MY_KEY}&returnType=json&dataTerm=Daily&stationName=%EC%95%84%EC%A3%BC%EB%8F%99'
+    save_file_path = 'data/observatory_file.txt'
+
+    response = rq.get(url)
+    data = response.json()
+
+    return data['response']['body']['items'][0]['khaiValue']
+
+if __name__ == '__main__' :
+    #시험용 코드
+    print(get_dust())
